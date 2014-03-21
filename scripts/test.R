61e92e148ee94fb70e264ce15a6aebb7b8b66f9c
Aung New PC from INSEAD <myintthein.aung@insead.edu>
2014-03-21 18:14:03 +0800

    added more codes

--- scripts/test.R
+++ scripts/test.R
@@ -11,3 +11,14 @@ hist(bills[bills$HOSPITALBILL > 500 & bills$HOSPITALBILL < 2000, "HOSPITALBILL"]
 table(bills[bills$HOSPITALBILL < 20, "HOSPITALBILL"])
 
 head(bills[bills$HOSPITALBILL == 0, ])
+
+bills <- bills[order(bills$ID, bills$yyyy, bills$DATEOFEVENT, bills$HOSPITALBILL, decreasing=F),]
+
+head(bills, 30)
+
+big.ID <- bills[bills$ID=="C000003554",]
+sum(big.ID$HOSPITALBILL)
+
+length(unique(big.ID$HRN))
+
+aggregate(HOSPITALBILL ~ HRN , big.ID, sum)
