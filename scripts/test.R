4cfbd6aa93a47f424eecb3ebe4b229451c11f21e
Aung New PC from INSEAD <myintthein.aung@insead.edu>
2014-03-25 16:17:41 +0800

    added more exploration data

--- scripts/test.R
+++ scripts/test.R
@@ -97,6 +97,37 @@ unique <- union(union(union(onlyWARDTYPE, bothBILLCATWARDTYPE), onlyBILLCAT), ne
 
 trainData <- aggregate(HOSPITALBILL ~ ID + AGE + GENDER + HRN + HOSPITAL + DIAGNOSUS + BILLCAT + DURATIONOFSTAY, bills, mean)
 trainData <- trainData[order(trainData$HRN),]
+trainData[, "inpatient"] <- ifelse(trainData[, "BILLCAT"] == "IN", 1, 0)
+trainData[, "outpatient"] <- ifelse(trainData[, "BILLCAT"] == "OU", 1, 0)
+trainData[, "surgpatient"] <- ifelse(trainData[, "BILLCAT"] == "DY", 1, 0)
+
+estimation_data_ids <- sample.int(nrow(trainData), 5000)
+estimation_data <- trainData[estimation_data_ids, ]
+
+non_estimation_data <- setdiff(1:nrow(trainData),estimation_data_ids)
+test_data <- trainData[non_estimation_data, ]
 
 
-lm1 <- lm(HOSPITALBILL ~ ID + AGE + GENDER + HRN + HOSPITAL + DIAGNOSUS + BILLCAT + DURATIONOFSTAY, data=trainData)
+#estimation_data[, "logbill"] <- ifelse(estimation_data[, "HOSPITALBILL"] == 0, 0, log(estimation_data[, "HOSPITALBILL"]))
+estimation_data[, "sqrtbill"] <- sqrt(estimation_data[, "HOSPITALBILL"])
+estimation_data[, "qutbill"] <- estimation_data[, "HOSPITALBILL"]^(1/2)
+hist(estimation_data[, "HOSPITALBILL"])
+hist(estimation_data[, "logbill"])
+hist(estimation_data[, "qutbill"])
+
+#lm1 <- lm(qutbill ~ AGE + GENDER + BILLCAT + DURATIONOFSTAY, data=estimation_data)
+lm1 <- lm(qutbill ~ AGE + GENDER + BILLCAT + DURATIONOFSTAY + inpatient + outpatient + surgpatient, data=estimation_data)
+
+#family=poisson()
+
+prediction <- predict(lm1, type="response", newdata=test_data[1:4000, c("AGE", "GENDER", "BILLCAT", "DURATIONOFSTAY", "inpatient", "outpatient", "surgpatient")])
+hist(prediction)
+hist((prediction)^2)
+
+tes <- cbind(test_data[1:4000,], (prediction)^2)
+#library(Metrics)
+
+rmsle(tes$HOSPITALBILL, tes[,"(prediction)^2"])
+
+sort(setdiff(unique(bills$HOSPITAL), unique(predict$HOSPITAL)))
+sort(setdiff(unique(predict$HOSPITAL), unique(bills$HOSPITAL)))
