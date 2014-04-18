1b0917f0acac063fb740ae3a2d5a22a90c64dc57
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2014-04-18 11:44:16 +0800

    added more submission

---
+++ scripts/18 Submissions.R
@@ -0,0 +1,39 @@
+### testing with association
+## testing 1 : if wardtype == "", logbill = log(170) but will use bills data. RMSLE 1.39841
+## testing 1 : bills data. RMSLE    1.67038
+## testing 2 : if wardtype == "", logbill = log(170) but will use bills data and itemNo as new variable. RMSLE 1.61055
+## testing 2 : bills data and itemNo as new variable. RMSLE 1.61055
+
+BILLCATCODE <- data.frame(BILLCATCODE=c(1:4), BILLCAT=unique(bills$BILLCAT))
+submission4 <- data.frame()
+
+trainData <- bills
+
+trainData[, "logbillb"] <- apply(trainData, 1, function(row) ifelse(row["WARDTYPE"] == "", log(170), as.numeric(row["logbill"])))
+
+for(i in 1:4){
+  for(j in 1:4){
+    lmfunction <- lm(logbill ~ AGE + GENDER + DURATIONOFSTAY + YEAROFADM + AGE*GENDER + GENDER*DURATIONOFSTAY + ItemNo +
+                       Cancer.Specialist + Community.Hospital + Dental + ENT + Eye + Kidney + Others + Oversea.Hospital + Private.Hospital + Public.Hospital + 
+                       Public.Specialist + Specialist + Surgery + Ward. + Ward.A + Ward.B + Ward.C + Ward.D + Ward.E + Ward.F + Ward.G + Ward.H + Ward.I + Ward.K + Ward.M + Ward.N + Ward.O + Ward.P +
+                       Diseases.of.the.genitourinary.system + Symptoms.signs.and.abnormal.clinical.and.laboratory.findings.not.elsewhere.classified + Diseases.of.the.nervous.system + Diseases.of.the.respiratory.system + Infectious.and.parasitic.diseases + Diseases.of.the.eye.and.adnexa + Diseases.of.the.sense.organs + Diseases.of.the.digestive.system + Diseases.of.the.circulatory.system + Diseases.of.the.skin.and.subcutaneous.tissue + Injury.poisoning.and.certain.other.consequences.of.external.causes + Diseases.of.the.musculoskeletal.system.and.connective.tissue + Neoplasms + Mental.and.behavioural.disorders + Factors.influencing.health.status.and.contact.with.health.services + Endocrine.nutritional.and.metabolic.diseases + Others + Diseases.of.the.ear.and.mastoid.process + Diseases.of.the.blood.and.bloodforming.organs.and.certain.disorders.involving.the.immune.mechanism + Pregnancy.childbirth.and.the.puerperium + Congenital.malformations.deformations.and.chromosomal.abnormalities + External.causes.of.morbidity.and.mortality + Certain.conditions.originating.in.the.perinatal.period
+                     , data=trainData[trainData$AGEGROUP == i & trainData$BILLCAT == BILLCATCODE[j,2],])
+    
+    test_data <- predict[predict$AGEGROUP == i & predict$BILLCAT == BILLCATCODE[j,2],]
+    
+    prediction <- predict(lmfunction, type="response", newdata=test_data)
+    tes <- cbind(test_data, exp(prediction)-1)
+    colnames(tes)[ncol(tes)] <- "prediction"
+    
+    submission4 <- rbind(submission4, tes)
+  }
+}
+
+submission4 <- submission4[, c(2:6,1,7:13, 76)]
+colnames(submission4)[ncol(submission4)] <- "HOSPITALBILL"
+submission4 <- submission4[order(submission4$UID),]
+submission4["HOSPITALBILL"] <- apply(submission4, 1, function(row) ifelse(as.numeric(row["HOSPITALBILL"]) < 0, 0, as.numeric(row["HOSPITALBILL"])))
+
+#submission4[, "HOSPITALBILL"] <- apply(submission4, 1, function(row) ifelse(row["WARDTYPE"] == "", 280, as.numeric(row["HOSPITALBILL"])))
+
+write.csv(submission4, "submission18d_Aung_Myint_Thein.csv", quote=FALSE, row.names=FALSE)
